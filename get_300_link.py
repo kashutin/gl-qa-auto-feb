2ee28ca1df50fd7dee52af689db27ea3fce86042
Andrii Kashutin <andrii.kashutin@gmail.com>
2022-04-04 20:16:48 +0300

    modified:   get_300_link.py

--- get_300_link.py
+++ get_300_link.py
@@ -1,10 +1,10 @@
 import requests
 url = input('Введіть адресу(bit.ly, goo.gl, etc): ')
 
-def recieve_url_from_redirect(url):
+def recieve_url_from_shortener(url):
     r = requests.get(url)
-    print("STATUS CODE OF PROVEDED URL: " +str(r.status_code))
-    print("STATUS CODE IN HEADER: " + str(r.history))
+    print("STATUS CODE OF SHORTENER: " +str(r.status_code))
+    print("STATUS CODE IN REQUEST HISTORY: " + str(r.history))
     fullurl = r.url
     return fullurl
 full_url = recieve_url_from_redirect(url)
