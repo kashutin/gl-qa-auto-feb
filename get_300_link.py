6fdd82cb076ed9d7c867fc270736a1c5582491c7
Andrii Kashutin <andrii.kashutin@gmail.com>
2022-04-04 20:29:23 +0300

    modified:   get_300_link.py

--- get_300_link.py
+++ get_300_link.py
@@ -1,12 +1,12 @@
 import requests
 url = input('Enter shortener URL(bit.ly, goo.gl, etc): ')
 
-def recieve_url_from_shortener(url):
+def get_url_from_shortener(url):
     r = requests.get(url)
     print("STATUS CODE OF SHORTENER: " +str(r.status_code))
     print("STATUS CODE IN REQUEST HISTORY: " + str(r.history))
     fullurl = r.url
     return fullurl
 
-full_url = recieve_url_from_shortener(url)
+full_url = get_url_from_shortener(url)
 print('FULL URL FROM THE PROVIDED SHORTENER LINK: \n' + full_url)
