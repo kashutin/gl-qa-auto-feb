b617b16f029d9bb9ccb2fde2e0c875119304eb5d
Andrii Kashutin <andrii.kashutin@gmail.com>
2022-04-19 09:05:49 +0300

    Automated QA Homework 3

--- cosmosid_requests.py
+++ cosmosid_requests.py
@@ -4,23 +4,23 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(levelname)s: %(me
 
 login_url = "https://app.cosmosid.com/api/v1/login"
 login_payload = {"expiry": 86400, "login_from": "login page"}
-login_headers = {"Authorization": "Basic bWFpbGludGVzdEB1a3IubmV0Om1haWxpbnRlc3RAdWtyLm5ldA=="}
+login_header = {"Authorization": "Basic bWFpbGludGVzdEB1a3IubmV0Om1haWxpbnRlc3RAdWtyLm5ldA=="}
 rf_url = "https://app.cosmosid.com/api/metagenid/v2/files?"
 rf_params = {"breadcrumbs": "1", "offset": "0", "limit": "1000", "folder_id": "84c966d5-8dce-429d-8f92-44d5e28b1581"}
 
 
-def login():
+def get_a_token():
     r=requests.post(login_url,
                     json=login_payload,
-                    headers=login_headers)
+                    headers=login_header)
     access_token = r.json()['token']
     logging.info(r.raise_for_status)
     return access_token
 
-def root_folder():
+def get_root_folder():
     r = requests.get(rf_url, headers={"x-token": a_token}, params=rf_params)
     logging.info(r.json())
 
 if __name__ == "__main__":
-    a_token = login()
-    root_folder()
+    a_token = get_a_token()
+    get_root_folder()
